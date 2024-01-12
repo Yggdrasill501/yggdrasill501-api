b2320c501bb6d0f869db6ebad0d0e087cfd58e7e
Filip Žitný <filipzitny@gmail.com>
2024-01-12 17:39:39 +0100

    Init

---
+++ src/test/java/net/yggdrasill501/portfolio/PersonalPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package net.yggdrasill501.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalPortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
