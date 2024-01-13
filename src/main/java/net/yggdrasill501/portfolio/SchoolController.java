3eaa38e9800803cb245547464f7e3eb063407533
Filip Žitný <filipzitny@gmail.com>
2024-01-14 00:01:25 +0100

    Education

--- src/main/java/net/yggdrasill501/portfolio/SchoolController.java
+++ src/main/java/net/yggdrasill501/portfolio/SchoolController.java
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/api/education")
 public class SchoolController {
