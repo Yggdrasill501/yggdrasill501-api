ec582dad52aaad4cdb22d2e0011417e320849f72
Filip Žitný <filipzitny@gmail.com>
2024-01-13 23:54:57 +0100

    school controller

--- src/main/java/net/yggdrasill501/portfolio/SchoolController.java
+++ src/main/java/net/yggdrasill501/portfolio/SchoolController.java
@@ -1,8 +1,13 @@
 package net.yggdrasill501.portfolio;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/education")
@@ -10,11 +15,11 @@ public class SchoolController {
     @Autowired
     private SchoolService schoolService;
     @GetMapping
-    private ResponseEntity<List<School>> getAllEducation() {
-        return new ResponseEntity<>(educationService.allEducation(), HttpStatus.OK);
+    private ResponseEntity<List<School>> getAllSchools() {
+        return new ResponseEntity<>(schoolService.allSchools(), HttpStatus.OK);
     }
     @GetMapping("/{schoolId}")
     public ResponseEntity<Optional<School>> getSingleSchool(@PathVariable String schoolId){
-        return new ResponseEntity<Optional<School>>(movieService.singleMovie(imdbId), HttpStatus.OK);
+        return new ResponseEntity<Optional<School>>(schoolService.singleSchool(schoolId), HttpStatus.OK);
     }
 }
