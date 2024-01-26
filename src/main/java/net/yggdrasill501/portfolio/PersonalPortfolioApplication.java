a12d9bf7251626d257fe97b16a2e5c92f5c03ff1
Filip Žitný <filipzitny@gmail.com>
2024-01-26 20:31:42 +0100

    Yggdrasill501's Personal Portfolio is being under construction.

--- src/main/java/net/yggdrasill501/portfolio/PersonalPortfolioApplication.java
+++ src/main/java/net/yggdrasill501/portfolio/PersonalPortfolioApplication.java
@@ -2,11 +2,19 @@ package net.yggdrasill501.portfolio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class PersonalPortfolioApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PersonalPortfolioApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String hello() {
+		return "Yggdrasill501's Personal Portfolio is being under construction.";
 	}
 }
